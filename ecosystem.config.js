0593b8d8b752df49258c1e1b8e6509ca3129cc48
Fiqri Maulana A <fiqri.maulana@datasintesa.id>
2024-08-08 12:13:11 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,5 +2,19 @@ module.exports = {
   apps: [{
     name: 'dadjokes',
     script: './server.js',
-  }]
+  }],
+
+    // Deployment Configuration
+  deploy : {
+    production : {
+      "user": "fiqri",
+      "host": ["localhost"],
+      "ref": "origin/main",
+      "repo": "git@github.com:fiqri-m/dadjokes.git",
+      "path": "/var/www/tes",
+      "post-deploy": "pm2 start ecosystem.config.js",
+      "pre-deploy": "npm install",
+      "key": "/home/fiqri/.ssh/id_rsa"
+    }
+  }
 }
